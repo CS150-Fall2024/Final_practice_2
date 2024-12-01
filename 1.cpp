ecca25c3f8d625e05001001968f72b0d2e93819a
Joseph Moran <tjmoran@nic.edu>
2024-12-01 11:20:48 -0800

    Problem 1 complete

--- 1.cpp
+++ 1.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 const int SIZE = 20;
 //function prototype
+int findLargest(double array[], int size);
 
 int main()
 {
     double numbers[SIZE] ={3.4, 5.7, 9.0, -3.2, 1.7, 3.56, 11.03, -3.78, 4.8, 0.0,
                             1.5, 6.09, -2.34, 10.03, 9.8, 2.45, 3.76, 8.9, -1.2, 4.8};
     //call findLargest and display the return
+    int largestIndex = findLargest(numbers, SIZE);
+    cout << "The index of the largest element in the array is " <<largestIndex<<endl;
 
     return 0;
 }
@@ -18,4 +21,15 @@ int main()
 *and the number of items in the array as parameters,
 *and returns the index of the largest item.
 *********************************************************************/
-
+int findLargest(double array[], int size)
+{
+    int maxIndex = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i] > array[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
