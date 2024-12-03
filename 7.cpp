fa40a442bd868c5c704a326bc4966f362f05a0d6
Joseph Moran <tjmoran@nic.edu>
2024-12-03 11:11:11 -0800

    problem 7 done

--- 7.cpp
+++ 7.cpp
@@ -8,9 +8,12 @@ a.	(5 pts) Define a structure called Movie which has 3 member variables.
 The member variables include title, director, and year.
 The title and director are strings and the year is an integer.
 */
-
-
-
+struct Movie
+{
+    string title;
+    string director;
+    int year;
+};
 
 /**
 b.	(5 pts) In the main() function, declare an array of type Movie
@@ -20,6 +23,10 @@ assign the values "Harry Potter and the Goblet of Fire" as the title,
 Display the fifth element of array*/
 int main()
 {
-
+    Movie myMovies[10];
+    myMovies[4].title = "Harry Potter and the Goblet of Fire";
+    myMovies[4].director = "Mike Newell";
+    myMovies[4].year = 2005;
+    cout << "Fifth movie: " << myMovies[4].title << ", " << myMovies[4].director << ", " << myMovies[4].year << endl;
     return 0;
 }
