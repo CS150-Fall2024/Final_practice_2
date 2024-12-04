0ca9caf7fdab033bd6c8ef315e18f1865df02794
Joseph Moran <tjmoran@nic.edu>
2024-12-04 12:25:04 -0800

    problem 11 done

--- 11.cpp
+++ 11.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //function prototypes
-void displayVector(vector <int> &vec);
+void displayVector(const vector <int> &vec);
 
 /**
 Write the C++ code that removes the duplicate values in a vector of integers.
@@ -14,11 +14,52 @@ The final vector should be sorted. You can write the solution code in main()
 int main()
 {
     vector <int> numbers={101,2,44,44,46,1,2,44,1};
+    cout << "Initial array: ";
     displayVector(numbers);
 
+    //remove duplicates
+    for(size_t i = 0; i < numbers.size()-1; i++)
+    {
+        for(size_t j = i+1; j < numbers.size(); j++)
+        {
+            if(numbers[i] == numbers[j])
+            {
+                int temp = numbers[j];
+                numbers[j] = numbers[numbers.size()-1];
+                numbers[numbers.size()-1] = temp;
+                numbers.pop_back();
+                j--;
+                continue;
+            }
+        }
+    }
+
+    cout << "Array without duplicate values: ";
+    displayVector(numbers);
+
+    //sort vector
+    bool madeAswap = true;
+    int lastIndex = numbers.size() - 1;
+
+    while (madeAswap)
+    {
+        madeAswap = false;
+        for (int count = 0; count < lastIndex; ++count)
+        {
+            if (numbers[count] > numbers[count + 1])
+            {
+                int temp = numbers[count];
+                numbers[count] = numbers[count + 1];
+                numbers[count + 1] = temp;
+                madeAswap = true;
+            }
+        }
+        --lastIndex;
+    }
 
 
-
+    cout << "Sorted array: ";
+    displayVector(numbers);
 
     return 0;
 }
@@ -26,7 +67,7 @@ int main()
 /********************displayVector*****************************
 * This function displays the vector of int (need for testing)
 *************************************************************/
-void displayVector(vector <int> &vec)
+void displayVector(const vector <int> &vec)
 {
     for (int i = 0; i <(int)vec.size(); i++)
     {
