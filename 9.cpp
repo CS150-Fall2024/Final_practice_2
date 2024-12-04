42ba1e0293327163f5afa3923034d32f24b0ee62
Joseph Moran <tjmoran@nic.edu>
2024-12-04 11:23:37 -0800

    problem 9 done

--- 9.cpp
+++ 9.cpp
@@ -11,7 +11,7 @@ int main()
     string input;
     cout << "Enter the string and I will tell if it is a palindrome: " << endl;
     getline(cin,input);
-    if (isPalindrome(input))
+    if(isPalindrome(input))
         cout <<"\nIt is a palindrome\n";
     else
         cout <<"\nIt is not a palindrome\n";
@@ -24,4 +24,24 @@ This function returns true if the string is a palindrome,
 otherwise false. A palindrome is a word or phrase that reads
 the same backward as forward (e.g. madam, nurses run, etc.).
 Note: string can have spaces for example, "taco cat".**/
-
+bool isPalindrome(string phrase)
+{
+    string newPhrase;
+    for(size_t i = 0; i < phrase.length(); i++)
+    {
+        //clean string
+        if(phrase[i] != ' ')
+        {
+            newPhrase.push_back(phrase[i]);
+        }
+    }
+    //check for palindrome
+    for(size_t i = 0; i < newPhrase.length()/2; i++)
+    {
+        if(newPhrase[i] != newPhrase[(newPhrase.length()-1)-i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
