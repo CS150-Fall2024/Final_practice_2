302c02c8a2e554eb55bc9a66f83ef98eeb01e8f4
Joseph Moran <tjmoran@nic.edu>
2024-12-04 18:43:14 -0800

    problem 12 done

--- 12.cpp
+++ 12.cpp
@@ -8,6 +8,7 @@ The function should return the value of the unique element.
 using namespace std;
 //function prototypes
 void displayArray(const int array[], int size);
+int unique(int array[], int size);
 
 
 const int SIZE = 13;
@@ -17,7 +18,7 @@ int main()
     int numbers[SIZE] ={1,7, 3, 0, 5, 6, 7, 9, 9, 1, 0, 6, 5};
     displayArray(numbers, SIZE);
     //call the unique function and display the output to the screen
-    cout<<"The unique element is "<<   <<endl;
+    cout<<"The unique element is "<< unique(numbers, SIZE) <<endl;
 
     return 0;
 }
@@ -26,6 +27,36 @@ that accepts an array of integers and the size of the array,
 where all elements but one occur twice.
 The function should return the value of the unique element.
 **/
+int unique(int array[], int size)
+{
+    //bool foundVal = false;
+    //int matchValue = 0;
+    int uniqueVal;
+    for(int i = 0; i < size-1; i++)
+    {
+        int matchValue = 0;
+        for(int j = 0; j < size; j++)
+        {
+            if(i==j)
+            {
+                continue;
+            }
+            if(array[i] == array[j])
+            {
+                matchValue++;
+            }
+            // uniqueVal = array[i];
+            // foundVal = true;
+        }
+        if(matchValue == 0)
+        {
+            uniqueVal = array[i];
+            break;
+        }
+    }
+
+    return uniqueVal;
+}
 
 
 
