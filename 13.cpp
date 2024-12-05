6a2b4ba87952f5a70bbc8c22ac3ea8439526935c
Joseph Moran <tjmoran@nic.edu>
2024-12-04 20:37:04 -0800

    problem 13 done

--- 13.cpp
+++ 13.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 4;
-
+//const int SIZE = 4;
+bool isPangram(string str);
 
 int main()
 {
@@ -11,8 +11,27 @@ int main()
     string str2 = "Jumbling vext frowzy hacks PDQ";//true
     string str3 = "It is a beautiful day at the Coeur dAlene"; //false
     //call the function
-
-
+    if(isPangram(str1))
+    {
+        cout << "String 1 is a pangram\n";
+    }
+    else{
+        cout << "String 1 is not a pangram\n";
+    }
+    if(isPangram(str2))
+    {
+        cout << "String 2 is a pangram\n";
+    }
+    else{
+        cout << "String 2 is not a pangram\n";
+    }
+    if(isPangram(str3))
+    {
+        cout << "String 3 is a pangram\n";
+    }
+    else{
+        cout << "String 3 is not a pangram\n";
+    }
 
     return 0;
 }
@@ -22,3 +41,32 @@ int main()
 * Write a function called isPangram, that accepts a string.
 * The function should return true if the string contain a pangram in the English alphabet and false otherwise
 **/
+bool isPangram(string str)
+{
+    //int count = 0;
+    for(int i = 97; i < 123; i++)
+    {
+        int count = 0;
+        for(size_t j = 0; j < str.length(); j++)
+        {
+            if(str[j] == i || str[j] == i-32)
+            {
+                count++;
+                continue;
+            }
+        }
+        if(count == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+    // if(count >= 26)
+    // {
+    //     return true;
+    // }
+    // else
+    // {
+    //     return false;
+    // }
+}
